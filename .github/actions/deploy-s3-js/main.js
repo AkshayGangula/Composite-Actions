ba6fd1181fdac13275ce95d1157eb826aaf7a37e
Gangula <axayreddyg@gmail.com>
2025-01-10 13:58:18 -0600

    Displaying URL

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,6 +1,6 @@
-const core = require('@actions/core')
+const core = require('@actions/core');
 // const github = require('@actions/github')
-const exec = require('@actions/exec')   
+const exec = require('@actions/exec');   
 function run() {
     // 1. Get some input vals
     const bucket = core.getInput('bucket', {required: true});
@@ -9,9 +9,10 @@ function run() {
 
 
     // 2. Upload Files
-    const s3Uri = `s3://${bucket}`
+    const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my custom javascript action');
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 run();
