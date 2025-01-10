31f5120cb5b21cfde1dad220efbe734b015635d7
Gangula <axayreddyg@gmail.com>
2025-01-10 12:50:30 -0600

    new changes in deploy.yml

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,7 +1,17 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 const exec = require('@actions/exec')   
 function run() {
-    core.notice('Hello from my custom javascript action')
+    // 1. Get some input vals
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRequired = core.getInput('bucket-region', {required: false});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+
+    // 2. Upload Files
+    const s3Uri = `s3://${bucket}`
+    exec.exec('aws s3 sync ${distFolder} ${s3Ui} --region ${bucketRegion}');
+
+    core.notice('Hello from my custom javascript action');
 }
 run();
